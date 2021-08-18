4fa546dfa86d224d40cfe49a9fd67645146ed24c
ivan-stepalin <uncabron121@yandex.ru>
2021-08-18 23:01:47 +0300

    level-3: add registration and login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Switch, useHistory} from "react-router";
+import {Route, Switch, useHistory, useRouteMatch} from "react-router";
 import {useEffect, useState} from "react";
 import {Header} from "../Header/Header";
 import {Main} from "../Main/Main";
@@ -11,107 +11,260 @@ import {Profile} from "../Profile/Profile";
 import {NotFound} from "../NotFound/NotFound";
 import {Movies} from "../Movies/Movies";
 import {SavedMovies} from "../SavedMovies/SavedMovies";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
+import {ProtectedRoute} from "../ProtectedRoute/ProtectedRoute";
+import MainApi from "../../utils/MainApi";
+import {MORE_BUTTON_RESOLUTION_SETTINGS} from "../../utils/constants";
+import {useFormWithValidation} from "../../utils/customHooks";
 
 function App() {
     const history = useHistory();
+    const [currentUser, setCurrentUser] = useState({});
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorText, setErrorText] = useState('');
+    const [state, setState] = useState({
+        movieList: [],
+        savedMovieList: [],
+        movieSearch: '',
+        movieSearchSubmitClick: false,
+        savedMovieSearch: '',
+        movieSavedSearchSubmitClick: false,
+        filteredMoviesList: [],
+        filteredSavedMovieList: [],
+        numberMovies: MORE_BUTTON_RESOLUTION_SETTINGS.big.default,
+        addNumberMovies: MORE_BUTTON_RESOLUTION_SETTINGS.big.grow,
+        disableInputs: false,
+    })
+
+    const tokenCheck = (token) => {
+        return MainApi.checkUserToken(token)
+            .then((res) => {
+                if (res) {
+                    setCurrentUser(res);
+                    setIsLoggedIn(true);
+                    history.push('/movies');
+                } else {
+                    // Promise.reject();
+                }
+            })
+            .catch((err) => console.log(`Ошибка ${err.status}: ${err.message}`))
+    };
+
+    const handleTextError = (message) => {
+        setErrorText(message);
+        setTimeout(() => setErrorText(''), 5000);
+    }
 
     function handleClickSidebar() {
         setIsOpenSidebar(true)
-        console.log(`w`)
     }
 
     function handleClickCloseSidebar() {
         setIsOpenSidebar(false)
     }
 
-    function handleClickLogin() {
-        setIsLoggedIn(true)
-        history.push('/movies')
-    }
+    // function handleClickLogin() {
+    //     setIsLoggedIn(true)
+    //     history.push('/movies')
+    // }
 
-    useEffect(() => {
-        console.log(isOpenSidebar)
-        // setIsOpenSidebar(true)
+    // useEffect(() => {
+    //     console.log(isOpenSidebar)
+    //     setIsOpenSidebar(true)
+    //
+    // }, [])
 
-    }, [])
+    // const handleClickRegistration = () => history.push('/signin');
 
-    const handleClickRegistration = () => history.push('/signin');
+    const handleClickLogin = (data) => {
+        const {email, password} = data;
+        setState({...state, disableInputs: true});
+        return MainApi.login(email, password)
+            .then((data) => {
+                localStorage.setItem('jwt', data.token);
+                tokenCheck(data.token);
+            })
+            .catch((err) => {
+                if (err && err === 'Ошибка 401' ) {
+                    handleTextError('Неверный логин или пароль');
+                } else if (err && err === 'Ошибка 400') {
+                    handleTextError('Проверьте формат данных');
+                } else {
+                    handleTextError('Ошибка выполнения команды. Попробуйте снова.');
+                }
+            })
+            .finally(() => setState({...state, disableInputs: false}));
+    };
+
+    const handleClickRegistration = (data) => {
+        const {name, email, password} = data;
+        setState({...state, disableInputs: true});
+        return MainApi.register(name, email, password)
+            .then(() => {
+                return handleClickLogin({email, password});
+            })
+            .catch((err) => {
+                if (err && err === 'Ошибка 409') {
+                    handleTextError('Пользователь с таким email уже существует');
+                } else if (err.status && err.status === 'Ошибка 400') {
+                    handleTextError('Проверьте формат данных');
+                } else {
+                    handleTextError('Ошибка выполнения команды. Попробуйте снова.');
+                }
+            })
+            .finally(() => setState({...state, disableInputs: false}));
+    };
 
     const handleClickLogout = () => {
-        setIsLoggedIn(false)
-        history.push('/signin')
+        localStorage.removeItem('jwt');
+        setIsLoggedIn(false);
+        setCurrentUser(null);
+        history.push('/');
     };
 
     return (
-        <>
+        <CurrentUserContext.Provider value={currentUser}>
+            {
+                useRouteMatch(['/signin', '/signup']) ? '' : (
+                    <>
+                        <Header
+                            isLoggedIn={isLoggedIn}
+                            handleClickSidebar={handleClickSidebar}
+                        />
+                        <MobileMenu
+                            isOperSidebar={isOpenSidebar}
+                            handleClickCloseSidebar={handleClickCloseSidebar}
+                        />
+                    </>
+                )
+            }
             <Switch>
                 <Route exact path='/'>
-                    <Header
-                        isLoggedIn={isLoggedIn}
-                        handleClickSidebar={handleClickSidebar}
-                    />
-                    <Main
-                        isOpenSidebar={isOpenSidebar}
-                    />
-                    <Footer/>
+                    <Route exact path={'/'}>
+                        <Main
+                            isOpenSidebar={isOpenSidebar}
+                        />
+                    </Route>
                     <MobileMenu
                         isOpenSidebar={isOpenSidebar}
                         handleClickCloseSidebar={handleClickCloseSidebar}
                     />
                 </Route>
                 <Route path='/signin'>
-                    <Login handleClickLogin={handleClickLogin}/>
+                    <Login
+                        state={state}
+                        handleClickLogin={handleClickLogin}
+                        useFormWithValidation={useFormWithValidation}
+                        errorText={errorText}
+                    />
                 </Route>
                 <Route path='/signup'>
                     <Registration
-                        handleClickRegistr={handleClickRegistration}
+                        errorText={errorText}
+                        state={state}
+                        handleClickRegistration={handleClickRegistration}
+                        useFormWithValidation={useFormWithValidation}
                     />
                 </Route>
-                <Route path='/movies'>
-                    <Header
-                        handleClickSidebar={handleClickSidebar}
-                        isLoggedIn={isLoggedIn}
-                        handleClickLogout={handleClickLogout}
-                    />
-                    <Movies/>
-                    <Footer/>
-                    <MobileMenu
-                        isOpenSidebar={isOpenSidebar}
-                        handleClickCloseSidebar={handleClickCloseSidebar}
-                    />
-                </Route>
-                <Route path='/saved-movies'>
-                    <Header
-                        isLoggedIn={isLoggedIn}
-                        handleClickLogout={handleClickLogout}
-                    />
-                    <SavedMovies/>
-                    <Footer/>
-                    <MobileMenu
-                        isOpenSidebar={isOpenSidebar}
-                        handleClickCloseSidebar={handleClickCloseSidebar}
-                    />
-                </Route>
-                <Route path='/profile'>
-                    <Header
-                        isLoggedIn={isLoggedIn}
-                        handleClickLogout={handleClickLogout}
-                    />
-                    <Profile
-                        handleClickLogout={handleClickLogout}
-                    />
-                    <MobileMenu
-                        isOpenSidebar={isOpenSidebar}
-                        handleClickCloseSidebar={handleClickCloseSidebar}
-                    />
-                </Route>
+                <ProtectedRoute
+                    exect path={'/movies'}
+                    isLoggedIn={isLoggedIn}
+                    component={Movies}
+                    state={state}
+                    setState={setState}
+                    // handleClickSaveMovie={handleClickSaveMovie}
+                    // handleClickRemoveSavedMovie={handleClickRemoveSavedMovie}
+                    isLoading={isLoading}
+                    setIsLoading={setIsLoading}
+                />
+                <ProtectedRoute
+                    exact path='/saved-movies'
+                    isLoggedIn={isLoggedIn}
+                    component={SavedMovies}
+                />
+                <ProtectedRoute
+                    exact path='/profile'
+                    isLoggedIn={isLoggedIn}
+                    component={Profile}
+                    handleClickLogout={handleClickLogout}
+                />
                 <Route path='*'>
                     <NotFound/>
                 </Route>
+                {
+                    useRouteMatch(['/signin', '/signup', '/profile',]) ? '' : (<Footer/>)
+                }
             </Switch>
-        </>
+        </CurrentUserContext.Provider>
+        // <>
+        //     <Switch>
+        //         <Route exact path='/'>
+        //             <Header
+        //                 isLoggedIn={isLoggedIn}
+        //                 handleClickSidebar={handleClickSidebar}
+        //             />
+        //             <Main
+        //                 isOpenSidebar={isOpenSidebar}
+        //             />
+        //             <Footer/>
+        //             <MobileMenu
+        //                 isOpenSidebar={isOpenSidebar}
+        //                 handleClickCloseSidebar={handleClickCloseSidebar}
+        //             />
+        //         </Route>
+        //         <Route path='/signin'>
+        //             <Login handleClickLogin={handleClickLogin}/>
+        //         </Route>
+        //         <Route path='/signup'>
+        //             <Registration
+        //                 handleClickRegistr={handleClickRegistration}
+        //             />
+        //         </Route>
+        //         <Route path='/movies'>
+        //             <Header
+        //                 handleClickSidebar={handleClickSidebar}
+        //                 isLoggedIn={isLoggedIn}
+        //                 handleClickLogout={handleClickLogout}
+        //             />
+        //             <Movies/>
+        //             <Footer/>
+        //             <MobileMenu
+        //                 isOpenSidebar={isOpenSidebar}
+        //                 handleClickCloseSidebar={handleClickCloseSidebar}
+        //             />
+        //         </Route>
+        //         <Route path='/saved-movies'>
+        //             <Header
+        //                 isLoggedIn={isLoggedIn}
+        //                 handleClickLogout={handleClickLogout}
+        //             />
+        //             <SavedMovies/>
+        //             <Footer/>
+        //             <MobileMenu
+        //                 isOpenSidebar={isOpenSidebar}
+        //                 handleClickCloseSidebar={handleClickCloseSidebar}
+        //             />
+        //         </Route>
+        //         <Route path='/profile'>
+        //             <Header
+        //                 isLoggedIn={isLoggedIn}
+        //                 handleClickLogout={handleClickLogout}
+        //             />
+        //             <Profile
+        //                 handleClickLogout={handleClickLogout}
+        //             />
+        //             <MobileMenu
+        //                 isOpenSidebar={isOpenSidebar}
+        //                 handleClickCloseSidebar={handleClickCloseSidebar}
+        //             />
+        //         </Route>
+        //         <Route path='*'>
+        //             <NotFound/>
+        //         </Route>
+        //     </Switch>
+        // </>
     );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,11 @@ import './Login.css';
 import logo from '../../images/login/logo.svg';
 
 export const Login = (props) => {
+    const { values, errors, isValid, handleChange } = props.useFormWithValidation({});
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.handleClickLogin(values);
+    };
     return (
         <main className='auth'>
             <div className='auth__container'>
@@ -11,17 +16,58 @@ export const Login = (props) => {
                 </Link>
                 <h2 className='auth__title'>Рады видеть!</h2>
                 <form className='auth__form'>
-                    <label className='auth__label'>E-mail</label>
-                    <input type='email' className='auth__input'/>
-                    <label className='auth__label'>Пароль</label>
-                    <input type='password' className='auth__input auth__input_error'/>
-                    <span className='auth__error'>Что-то пошло не так...</span>
-                    <button type='submit' className='button auth__submit' onClick={props.handleClickLogin}>Войти</button>
-                    <p className='auth__registr-text'>
-                        Ещё не зарегистрированы? <Link to='/signup' className='link auth__register-link'>Регистрация</Link>
-                    </p>
+                    <label className='auth__label' htmlFor='auth__email'>E-mail</label>
+                    <input
+                        type='email'
+                        id='auth__email'
+                        className={`auth__input${errors.email ? ' auth__input_error' : ''}`}
+                        name='email'
+                        value={values.email || ''}
+                        onChange={handleChange}
+                        disabled={props.state.disableInputs}
+                        required
+                    />
+                    {
+                        errors.email
+                            ? <span className='auth__error auth__error_input'>{errors.email}</span>
+                            : ''
+                    }
+                    <label className='auth__label' htmlFor='auth__password'>Пароль</label>
+                    <input
+                        type='password'
+                        id='auth__password'
+                        className={`auth__input${errors.password ? ' auth__input_error' : ''}`}
+                        name='password'
+                        value={values.password || ''}
+                        onChange={handleChange}
+                        disabled={props.state.disableInputs}
+                        required
+                    />
+                    {
+                        errors.password
+                            ? <span className='auth__error auth__error_input'>{errors.password}</span>
+                            : ''
+                    }
+                    <div className='auth__submit-block'>
+                        {
+                            props.errorText
+                                ? <span className='auth__error auth__error_submit'>{props.errorText}</span>
+                                : ''
+                        }
+                        <button
+                            type='submit'
+                            className='button auth__submit'
+                            onClick={handleSubmit}
+                            disabled={!isValid || props.state.disableInputs}
+                        >
+                            Войти
+                        </button>
+                        <p className='auth__registr-text'>
+                            Ещё не зарегистрированы? <Link to='/signup' className='link auth__register-link'>Регистрация</Link>
+                        </p>
+                    </div>
                 </form>
             </div>
         </main>
-    )
+    );
 }
